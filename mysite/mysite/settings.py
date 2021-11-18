795cfa94ea730cb83c07a611a6cc6a0c999688c9
Kirill Gubanov <kirill.gubanov21@gmail.com>
2021-11-18 14:00:29 +0100

    Create custom user model

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'phonenumber_fields',
+    'users.apps.UsersConfig'
 ]
 
 ROOT_URLCONF = 'mysite.urls'
--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,6 +1,71 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 
-class User(AbstractBaseUser):
-    pass
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, username, password, **extra_fields):
+        values = [email, username]
+        field_value_map = dict(zip(self.model.REQUIRED_FIELDS, values))
+
+        # check if all fields are set
+        for field_name, value in field_value_map.items():
+            if not value:
+                raise ValueError(f'The {field_name} must be set.')
+
+            email = self.normalize_email(email)
+            user = self.model(email=email, username=username, **extra_fields)
+            user.set_password(password)
+            user.save(using=self._db)
+            return user
+
+    def create_user(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(email, username, password, **extra_fields)
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, username, password, **extra_fields)
+
+
+class UserType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+# Custom user model
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=255)
+    last_login = models.DateTimeField(null=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_activated = models.BooleanField(default=False)
+    user_type = models.ForeignKey(UserType, on_delete=models.PROTECT)
+
+    objects = UserManager
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = PhoneNumberField(unique=True)
+    date_birthday = models.DateField()
+    user = models.OneToOneField(User, on_delete=models.PROTECT, primary_key=True)
