6e8cf6e09eba68bb6832288db165730a0229abcc
Kirill Gubanov <kirill.gubanov21@gmail.com>
2021-11-22 15:49:00 +0100

    Create models for advertisments app

--- mysite/advertisements/models.py
+++ mysite/advertisements/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.postgres.fields import JSONField
 from django.conf import settings
 
+import hashlib
+import os
+
 
 class AdvertisingSpaceType(models.Model):
     name = models.CharField(max_length=255)
@@ -24,6 +27,41 @@ class AdvertisingSpace(models.Model):
     )
 
 
-class AdvertisingSpaceImage(models.Model):
-    image =
+# functions for correctly storing images in AdvertisingSpaceImage model
+def path_and_rename(instance, filename, deep_level=3):
+    """
+    Create whole filepath string to the storing image based on changed filename
+    :param instance: instance of class
+    :param filename: filename before editing
+    :param deep_level: number of levels of nesting of folders
+    :return: string of the whole path to the storing image
+    """
+    path = 'photos/'
+    count_letters = 2
+    filename = get_md5_file(filename)
 
+    for i in range(deep_level):
+        path += filename[count_letters - 2:count_letters] + '/'
+        count_letters += 2
+
+    return os.path.join(path, filename)
+
+
+def get_md5_file(filename):
+    """
+    Create md5 hash string based on filename
+    :param filename: filename
+    :return: string of md5 hash
+    """
+    result = hashlib.md5(filename.encode()).hexdigest()
+
+    return result
+
+
+class AdvertisingSpaceImage(models.Model):
+    image = models.ImageField(upload_to=path_and_rename, verbose_name='Image')
+    adverting_space = models.ForeignKey(
+        AdvertisingSpace,
+        on_delete=models.PROTECT,
+        verbose_name='Adverting space'
+    )
