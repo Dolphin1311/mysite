dadaf47f90c8b4ead58ee1ce765a9fe8a84f76b6
unknown <kirill.gubanov21@gmail.com>
2022-01-13 18:24:11 +0100

    Commit before push

--- mysite/advertisements/utils.py
+++ mysite/advertisements/utils.py
@@ -5,7 +5,5 @@ class DataMixin:
     @staticmethod
     def get_user_context(**kwargs):
         context = kwargs
-        adv_types = AdvertisingSpaceType.objects.all()
-        context['adv_types'] = adv_types
 
         return context
