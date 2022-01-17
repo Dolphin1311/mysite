f65dda77021f5376d83cb4af97680412faec99e3
unknown <kirill.gubanov21@gmail.com>
2022-01-17 19:58:18 +0100

    Add neccessary files for user cabinet on adv spaces info

--- mysite/lib/templatetags/generic_tags.py
+++ mysite/lib/templatetags/generic_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.inclusion_tag('header.html')
 def show_header(user):
     return {'user': user}
+
+
+@register.inclusion_tag('user-cabinet-sidebar.html')
+def show_sidebar(user):
+    return {'user': user}
