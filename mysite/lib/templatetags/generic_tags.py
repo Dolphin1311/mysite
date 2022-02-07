ac500adbe88add2401215fb2605d6d2a5d543e42
unknown <kirill.gubanov21@gmail.com>
2022-02-07 19:08:33 +0100

    Change style of user cabinet

--- mysite/lib/templatetags/generic_tags.py
+++ mysite/lib/templatetags/generic_tags.py
@@ -6,14 +6,17 @@ register = template.Library()
 
 @register.inclusion_tag('header.html')
 def show_header(user):
+    # TODO: make this tag like show_cabinet_navigation tag
     return {'user': user}
 
 
-@register.inclusion_tag('user-cabinet-sidebar.html')
-def show_sidebar(user):
-    # check if user is person type
-    if user.user_type == UserType.objects.get(pk=2):
-        person = Person.objects.get(user=user)
-        return {'person_name': person.first_name, 'person_lastname': person.last_name}
-    else:
-        return {'user': user}
+@register.inclusion_tag('user-cabinet-navigation.html')
+def show_cabinet_navigation(nav_selected=0):
+    cabinet_navigation = [
+        {'title': 'Advertising Spaces', 'url': 'user_adv_spaces'},
+        {'title': 'Messages', 'url': 'user_messages'},
+        {'title': 'Personal data', 'url': 'user_data'},
+        {'title': 'Change password', 'url': 'user_change_pass'}
+    ]
+
+    return {'cabinet_nav': cabinet_navigation, 'cabinet_nav_selected': nav_selected}
--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', UserCabinetAdvSpacesView.as_view(), name='user_adv_spaces'),
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout')
+    path('logout/', logout_view, name='logout'),
+    # change here
+    path('messages/', logout_view, name='user_messages'),
+    path('data/', logout_view, name='user_data'),
+    path('change-password/', logout_view, name='user_change_pass'),
 ]
