2bf4e6906e3bf31f7d108c7d991bd2945ad08520
Kirill <kirill.gubanov21@gmail.com>
2022-02-13 19:23:49 +0100

    Create orders/forms.py. Create OrderItemForm.

---
+++ mysite/orders/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import OrderItem
+
+
+class OrderItemForm(forms.ModelForm):
+    def __init__(self, adv_space, **kwargs):
+        self._adv_space = adv_space
+        super(OrderItemForm, self).__init__(**kwargs)
+
+    class Meta:
+        model = OrderItem
+        fields = ["date_from", "date_to", "price"]
+        labels = {
+            "date_from": "Date from",
+            "date_to": "Date to",
+            "price": "Price per month"
+        }
+        widgets = {
+            "date_from": forms.DateInput(attrs={"id": "date-from"}),
+            "date_to": forms.DateInput(attrs={"id": "date-to"}),
+            "price": forms.NumberInput(attrs={"id": "price"})
+        }
+
+    def save(self, commit=True):
+        instance = super(OrderItemForm, self).save(commit=False)
+        instance.advertising_space = self._adv_space
+
+        if commit:
+            instance.save()
+
+        return instance
+
