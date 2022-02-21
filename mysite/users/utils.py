5195e122081299477c3f1bcaf4536769afd677f5
Kirill <kirill.gubanov21@gmail.com>
2022-02-21 14:26:27 +0100

    Add custom validator for checking if selected data is not in the future.

--- mysite/users/utils.py
+++ mysite/users/utils.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 from django.views.generic.base import ContextMixin
 
 
@@ -6,3 +8,9 @@ class DataMixin(ContextMixin):
         context = super(DataMixin, self).get_context_data(**kwargs)
 
         return context
+
+
+def no_future_date(value):
+    today = timezone.now().date()
+    if value > today:
+        raise ValidationError("Date of birthday can't be in future.")
