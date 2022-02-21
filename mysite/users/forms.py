19b88c4c7807ddafd108e77719c311b1534b2dd4
Kirill <kirill.gubanov21@gmail.com>
2022-02-21 14:24:03 +0100

    Create LoginForm with overriding clean method for getting email as username.

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User, Person
 
 
@@ -22,3 +23,18 @@ class PersonForm(forms.ModelForm):
             "phone": "Phone",
             "date_birthday": "Date of birthday",
         }
+
+
+class LoginForm(AuthenticationForm):
+    def clean(self):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+
+        if username is not None and password:
+            self.user_cache = authenticate(self.request, email=username, password=password)
+            if self.user_cache is None:
+                raise self.get_invalid_login_error()
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
+        return self.cleaned_data
