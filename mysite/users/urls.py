2620ac3810ba666a1d72058d05275f2a3e6f3d7b
Kirill <kirill.gubanov21@gmail.com>
2022-02-23 17:32:52 +0100

    Delete messages url. Add orders url.

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", logout_view, name="logout"),
     # change here
-    path("messages/", logout_view, name="user_messages"),
+    path("orders/", logout_view, name="user_orders"),
     path("data/", logout_view, name="user_data"),
     path("change-password/", logout_view, name="user_change_pass"),
 ]
