43a17db65fb337f0316c24e6cff6eb2b67ba5d49
Kirill <kirill.gubanov21@gmail.com>
2022-03-03 12:51:17 +0100

    Delete person form.

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -1,34 +1,15 @@
 from django import forms
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import User, Person, UserType
+from .models import User
 
 
 class UserForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super(UserForm, self).__init__(*args, **kwargs)
-        self.fields[
-            "user_type"
-        ].queryset = UserType.objects.all()
-
     class Meta:
         model = User
         fields = ["email", "user_type"]
         widgets = {"password": forms.PasswordInput()}
         labels = {"email": "E-mail", "user_type": "User type"}
-
-
-class PersonForm(forms.ModelForm):
-    class Meta:
-        model = Person
-        fields = ["first_name", "last_name", "phone", "date_birthday"]
-        widgets = {"date_birthday": forms.DateInput(attrs={"type": "date"})}
-        labels = {
-            "first_name": "First name",
-            "last_name": "Last name",
-            "phone": "Phone",
-            "date_birthday": "Date of birthday",
-        }
 
 
 class LoginForm(AuthenticationForm):
