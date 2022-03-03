fc65745c6cc0c29ef556430625ca9af7446d79b0
Kirill <kirill.gubanov21@gmail.com>
2022-03-03 12:50:22 +0100

    Delete UserType and Person models

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,12 +1,7 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils import timezone
-from .utils import no_future_date
-from phonenumber_field.modelfields import PhoneNumberField
-
-from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -35,15 +30,9 @@ class UserManager(BaseUserManager):
         return self._create_user(email, password, user_type, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
-        try:
-            user_type = UserType.objects.get(name="admin")  # get admin user type
-        except ObjectDoesNotExist:
-            user_type = UserType.objects.create(name="admin")
-
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
-        extra_fields.setdefault("user_type", user_type)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
@@ -51,17 +40,7 @@ class UserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        if extra_fields.get("user_type") != user_type:
-            raise ValueError("Must be admin user type for superuser.")
-
         return self._create_user(email, password, **extra_fields)
-
-
-class UserType(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return str(self.name).capitalize()
 
 
 # Custom user model
@@ -71,24 +50,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(default=timezone.now, verbose_name="Date joined")
     is_staff = models.BooleanField(default=False, verbose_name="Is stuff")
     is_active = models.BooleanField(default=False, verbose_name="Is activated account")
-    user_type = models.ForeignKey(
-        UserType, on_delete=models.PROTECT, verbose_name="User type"
-    )
 
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-
-
-class Person(models.Model):
-    first_name = models.CharField(max_length=255, verbose_name="First name")
-    last_name = models.CharField(max_length=255, verbose_name="Last name")
-    phone = PhoneNumberField(unique=True, verbose_name="Phone")
-    date_birthday = models.DateField(
-        verbose_name="Date of birth", validators=[no_future_date]
-    )
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
